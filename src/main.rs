a775d3dd506f735e214e007be170259f30757ab5
Lucas Antunes <lucasba8@gmail.com>
2020-03-31 09:20:02 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+use mqtt::packet::*;
+use mqtt::control::*;
+use mqtt::*;
+
+use std::env;
+
+use uuid::Uuid;
+
+use std::str;
+
+fn main()
+{
+    let broker_address = "localhost:1883";
+    let client_id = format!("/MQTT/rust/{}", Uuid::new_v4());
+    let topic_name = TopicName::new("test").unwrap();
+
+
+    let args: Vec<String> = env::args().collect();
+    let is_subscriber: bool = args.len() > 1 && args[1] == "--subscriber";
+
+
+    let mut stream = TcpStream::connect(broker_address).unwrap();
+
+    //Connection
+    let mut conn = ConnectPacket::new("MQTT", client_id);
+    conn.set_clean_session(true);
+
+    let mut buf = Vec::new();
+    conn.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+
+    let connack: ConnackPacket = ConnackPacket::decode(&mut stream).unwrap();
+
+    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted
+    {
+        panic!("failed to connect to server, {:?}", connack.connect_return_code());
+    }
+
+    println!("Connected!");
+
+
+    if is_subscriber
+    {
+        //Read packets
+
+        let topic_filter = TopicFilter::new("test").unwrap();
+        let sub_packet = SubscribePacket::new(10, vec![(topic_filter, QualityOfService::Level0)]);
+        
+        let mut buf = Vec::new();
+        sub_packet.encode(&mut buf).unwrap();
+        
+        stream.write_all(&buf[..]).unwrap();
+
+        loop
+        {
+            let packet = VariablePacket::decode(&mut stream).unwrap();
+
+            match packet
+            {
+                VariablePacket::PublishPacket(ref pkt) =>
+                {
+                    let payload = str::from_utf8(pkt.payload_ref()).unwrap();
+
+                    println!("Got something! {:?}", payload);
+                }
+
+                VariablePacket::SubackPacket(..) =>
+                {
+                    println!("Subscribed successfully!");
+                }
+
+                _ => {}
+            }
+        }
+    }
+    else
+    {
+        //Send packets
+
+        let mut counter = 0;
+
+        loop
+        {
+            println!("Send {:?}!", counter);
+
+            let msg = format!("Data from MQTT sensor {:?}", counter);
+            let pub_packet = PublishPacket::new(topic_name.clone(), QoSWithPacketIdentifier::Level0, msg);
+
+            let mut buf = Vec::new();
+            pub_packet.encode(&mut buf).unwrap();
+            stream.write_all(&buf[..]).unwrap();
+
+            counter += 1;
+
+            std::thread::sleep(std::time::Duration::from_millis(500));
+        }
+    }
+}
